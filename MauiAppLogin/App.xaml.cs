d842a5e12f6259a1fbfbba9abd3b8739b9d473af
Raul Pedrogan <rpm2007@uol.com.br>
2024-10-21 14:41:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppLogin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppLogin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppLogin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
